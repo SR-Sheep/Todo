37eae018e044ba1641e494b46df9fdb18a1ad35c
양성룡 <act4ysr@gmail.com>
2022-04-12 20:13:41 +0900

    필요없는 import 제거

--- src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -1,12 +1,10 @@
 package com.example.demo.config;
 
-import org.apache.catalina.filters.CorsFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CsrfFilter;
 
 import com.example.demo.security.JwtAuthenticationFilter;
@@ -46,6 +44,5 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 //		http.addFilterAfter(jwtAuthenticationFilter, BasicAuthenticationFilter.class);
 		http.addFilterAfter(jwtAuthenticationFilter, CsrfFilter.class);
 	}
-	
 	
 }
